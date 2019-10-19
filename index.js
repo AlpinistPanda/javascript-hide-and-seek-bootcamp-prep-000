aaa055270b7f393cfed994cc458ee805da2416d4
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-19 09:19:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector (selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div')
+}
+
+function increaseRankBy(n) {
+  const lis = document
+  .querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+
+  lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n).toString();
+}
+}
