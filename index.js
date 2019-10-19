11a40882e6893a0ce7780c550315d41fca93146d
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-19 09:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getFirstSelector (selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('ul#nested div div div div')
+  return document.getElementById('nested').querySelector('ul#nested div div div div')
 }
 
 function deepestChild() {
